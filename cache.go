5c0c36a466ddc225336287fc4c6d7527802316e7
diguacheng <467821528@qq.com>
2020-12-24 21:09:48 +0800

    2

---
+++ cache.go
@@ -0,0 +1,35 @@
+package gocache
+
+import (
+	"gocache/lru"
+	"sync")
+
+
+type cache struct {
+	mu sync.Mutex 
+	lru *lru.Cache 
+	cacheBytes int64 
+}
+
+func (c *cache)add(key string,value ByteView){
+	c.mu.Lock() 
+	defer c.mu.Unlock() 
+	if c.lru==nil{
+		//这种方法称之为延迟初始化(Lazy Initialization)，一个对象的延迟初始化意味着
+		//该对象的创建将会延迟至第一次使用该对象时。主要用于提高性能，并减少程序内存要求
+		c.lru=lru.New(c.cacheBytes,nil)
+	}
+	c.lru.Add(key,value)
+}
+
+func (c *cache)get(key string)(value ByteView,ok bool){
+	c.mu.Lock() 
+	defer c.mu.Unlock()
+	if c.lru==nil{
+		return
+	}
+	if v,ok:=c.lru.Get(key);ok{
+		return v.(ByteView),ok
+	}
+	return 
+}
